21532afe7766312dcf35ddc0e189e155fefc456b
이진석 <sonaky47@gmail.com>
2018-08-11 23:00:34 +0900

    update adm/index.php

--- adm/index.php
+++ adm/index.php
@@ -3,13 +3,6 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/adm/variables/indexVar.php' ?>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/adm/partials/header.php' ?>
 
-<div class="wrapper-logo">
-  <img src="/static/images/main/cherry-logo.svg" class="logo">
-</div>
-
-<h3 class="text-center title">
-</h3>
-
 <form class="container" ng-controller="formCtrl" ng-submit="submit()">
   <div class="form-group">
     <input type="text" class="form-control" ng-model="id" placeholder="아이디를 입력하세요">
@@ -20,4 +13,4 @@
   <button type="submit" class="btn btn-primary">로그인</button>
 </form>
 
-<?php include $_SERVER['DOCUMENT_ROOT'] . 'amd/partials/footer.php' ?>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/adm/partials/footer.php' ?>
